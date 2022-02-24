be5da81d2ba6f6aaf3d3074c650b3e49741d3dc7
Furkan Kapukaya <furkan.kapukaya@akinon.com>
2022-02-24 17:06:10 +0300

    remove test serializers

--- app/iska/forecasts/serializers.py
+++ app/iska/forecasts/serializers.py
@@ -1,5 +1,3 @@
-from django.contrib.gis.geos import Point
-
 from rest_framework import serializers
 
 from forecasts.models import Forecast
@@ -11,44 +9,6 @@ class BaseModelSerializer(serializers.ModelSerializer):
 
 
 class ForecastCreateSerializer(BaseModelSerializer):
-    #is_active = serializers.BooleanField()
-    #longitude = serializers.DecimalField(max_digits=19, decimal_places=10)
-    #latitude = serializers.DecimalField(max_digits=19, decimal_places=10)
-
-    class Meta:
-        model = Forecast
-        #fields = ('latitude', 'longitude', 'is_active', 'route_id')
-        fields = '__all__'
-
-    def somesome(self, validated_data):
-        longitude = float(validated_data["longitude"])
-        latitude = float(validated_data["latitude"])
-        is_active = validated_data["is_active"]
-        try:
-            username = self.context['request'].user.username
-        except:
-            raise serializers.ValidationError("Invalid user")
-
-        validated_data["username"] = username
-
-        point = Point(longitude, latitude)
-        validated_data["longitude"] = longitude
-        validated_data["latitude"] = latitude
-        point.srid = 4326
-        forecast = Forecast.objects.create(forecast=point, **validated_data)
-        if is_active:
-            Forecast.objects.filter(route_id=forecast.route_id).update(is_active=True)
-
-        return forecast
-
-
-class ForecastCSVSerializer(serializers.Serializer):
     class Meta:
         model = Forecast
         fields = '__all__'
-
-    def to_internal_value(self, data):
-        lat_val = data.get('myfield')
-        output = super(ForecastCSVSerializer, self).to_internal_value(data)
-        output['myfield'] = ','.join(myfield_val)
-        return output
--- app/iska/subscriptions/serializers.py
+++ app/iska/subscriptions/serializers.py
@@ -1,5 +1,3 @@
-from django.contrib.gis.geos import Point
-
 from rest_framework import serializers
 
 from subscriptions.models import Subscription
@@ -11,34 +9,6 @@ class BaseModelSerializer(serializers.ModelSerializer):
 
 
 class SubscriptionCreateSerializer(BaseModelSerializer):
-    #is_active = serializers.BooleanField()
-    #longitude = serializers.DecimalField(max_digits=19, decimal_places=10)
-    #latitude = serializers.DecimalField(max_digits=19, decimal_places=10)
-
     class Meta:
         model = Subscription
-        #fields = ('latitude', 'longitude', 'is_active', 'route_id')
         fields = '__all__'
-
-    def somesome(self, validated_data):
-        longitude = float(validated_data["longitude"])
-        latitude = float(validated_data["latitude"])
-        is_active = validated_data["is_active"]
-        try:
-            username = self.context['request'].user.username
-        except:
-            raise serializers.ValidationError("Invalid user")
-
-        validated_data["username"] = username
-
-        point = Point(longitude, latitude)
-        validated_data["longitude"] = longitude
-        validated_data["latitude"] = latitude
-        point.srid = 4326
-        subscription = Subscription.objects.create(subscription=point, **validated_data)
-        if is_active:
-            Subscription.objects.filter(route_id=subscription.route_id).update(is_active=True)
-
-        return subscription
-
-
